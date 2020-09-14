7d8fcc7b3cfd4c60f9444d8bae95662689e05de9
knapek <knapekk@gmail.com>
2020-09-14 12:35:34 +0200

    Ajax declaration fixed, and tested

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
-       $("div#main_box").append(`<p>Siemanko</p>`); 
+       $('#home').click(function () { 
+              
+              $('body').load("index.html", function (response, status, request) {
+                     alert(response);l
+                     
+              });
+              
+       });
+       $('#o_nas').click(function () { 
+              $('#removable').remove();
+              const createDiv = document.createElement('div');
+              $(createDiv).attr('id','removable');
+              $(createDiv).appendTo('#main_box');
+
+       });
 });
